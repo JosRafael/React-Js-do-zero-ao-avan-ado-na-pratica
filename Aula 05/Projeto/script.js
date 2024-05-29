1a67e25bbb1dc79ec537e3c8bad19fc7d3f2a8c8
JosRafael <rafaelmatiashue@gmail.com>
2024-05-29 15:57:38 -0300

    includes

--- Aula 05/Projeto/script.js
+++ Aula 05/Projeto/script.js
@@ -1,8 +1,12 @@
-function adicionar(...numeros){
-  let total = numeros.reduce((total, proximo) =>{
-    let soma = total + proximo;
-    return soma;
-  });
-  console.log(total);
+//includes
+//endsWith
+// startsWith
+
+let nomes = ['Matheus', 'Lucas', 'João']
+
+
+if(nomes.includes('Lucas')){
+  console.log('Lucas está na lista');
+}else{
+  console.log('Lucas não está na lista');
 }
-adicionar(1,2,3,4,5,6,7);
