b521529df7f950d83d5353f1f89584a4cc62319d
JosRafael <rafaelmatiashue@gmail.com>
2024-05-29 16:12:11 -0300

    every

--- Aula 05/Projeto/script.js
+++ Aula 05/Projeto/script.js
@@ -1,17 +1,17 @@
-//includes
-//endsWith
-// startsWith
+//SOME EVERY
 
-//let nomes = ['Matheus', 'Lucas', 'João']
+let nomes = [
+  {nome: 'Matheus', idade: 18},
+  {nome: 'Marias', idade: 25},
+  {nome: 'Henrique', idade: 20},
+]
 
-// console.log(nomes.includes('Luca'));
+//console.log(nomes.some(nome => nome === 'Henrique'));
 
-let nome = 'Matheus';
+//console.log(nomes.every(nome => nome.idade >= 18));
 
-// console.log(nome.endsWith('sa'));
-
-if(nome.startsWith('Mat')){
-  console.log('O *Mat* se escontra na lista de nomes');
+if(nomes.every(nome => nome.idade>=28)){
+  console.log("TODOS SÃO MAIORES QUE 18");
 }else{
-  console.log('O *Mat* não se escontra na lista de nomes');
+  console.log("NEM TODOS SÃO MAIORES QUE 18");
 }
