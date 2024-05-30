f890c169c83a73fcf1fb9ebb6eeecbaaa5de8b8e
JosRafael <rafaelmatiashue@gmail.com>
2024-05-29 23:35:50 -0300

    Eventos

--- Curso ReactJS/React JS/meuapp/src/App.js
+++ Curso ReactJS/React JS/meuapp/src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      hora: '00:00:00',
-     };
-  }
-componentDidMount(){
-  setInterval(() => {
-    this.setState({hora: new Date().toLocaleTimeString()});
-  }, 1000);
-}
-componentDidUpdate(){
-  setInterval(() => {
-    console.log('Atualizou!!!')
-  }, 1000);
-}
-
-
   render(){
     return(
       <div>
-        <h1>Meu projeto {this.state.hora}</h1>
+        <h1>
+          <Membro nome="Visitante" />
+        </h1>
       </div>
-    );
+    )
   }
 }
 
