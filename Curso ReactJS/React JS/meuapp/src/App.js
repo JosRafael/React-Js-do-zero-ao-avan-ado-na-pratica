a1d43b0004db8b5f42d8a575e3b81853629acc31
JosRafael <rafaelmatiashue@gmail.com>
2024-05-29 22:57:34 -0300

    props class extends

--- Curso ReactJS/React JS/meuapp/src/App.js
+++ Curso ReactJS/React JS/meuapp/src/App.js
@@ -1,40 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Equipe = (props) =>{
-  return(
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-    <Social fb={props.facebook}/>
-    <hr/>
-    </div>
-  )
+//div usada no react como tag pai
+
+class Equipe extends Component {
+    render(){
+      return(
+        <div>
+          <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+       <hr/>
+        </div>
+      );
+    }
 }
-const Sobre = (props) =>{
-  return(
-    <div>
-      <h2>Olá sou o(a) {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade} anos</h3>
-    </div>
-  )
-}
-const Social = (props) =>{
-  return(
-    <div>
-        <a href={props.fb}>Facebook </a>
-        <a>Linkedin </a>
-        <a>Youtube </a>
-    </div>
-  )
+class Sobre extends Component {
+    render(){
+      return(
+        <div>
+          <h2>Olá sou o {this.props.nome}!</h2>
+          <h3>Cargo: {this.props.cargo}</h3>
+          <h3>Idade: {this.props.idade} anos</h3>
+        </div>
+      );
+    }
 }
 
 function App() {
   return (
     <div>
-    <h1>Conheça a nossa equipe: </h1>
-    <Equipe nome="Matheus" cargo="Programador" idade="29" facebook="https://google.com"/>
-    <Equipe nome="Lucas" cargo="Design" idade="15" facebook="https://google.com"/>
-    <Equipe nome="Amanda" cargo="Front-End" idade="24" facebook="https://google.com"/>
+      <h1>Conheça a nossa equipe: </h1>
+      <Equipe nome="Matheus" cargo="Programador" idade="24"/>
     </div>
   );
 }
