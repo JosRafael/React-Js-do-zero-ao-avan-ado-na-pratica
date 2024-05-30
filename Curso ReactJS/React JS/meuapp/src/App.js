ace9e253ee4d182f6e81b03b6cd19171c609c6e5
JosRafael <rafaelmatiashue@gmail.com>
2024-05-29 23:25:05 -0300

    Ciclo de vidas

--- Curso ReactJS/React JS/meuapp/src/App.js
+++ Curso ReactJS/React JS/meuapp/src/App.js
@@ -1,44 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {
-      nome: "Matheus",
-      contador: 0,
-    };
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
+    this.state={
+      hora: '00:00:00',
+     };
   }
+componentDidMount(){
+  setInterval(() => {
+    this.setState({hora: new Date().toLocaleTimeString()});
+  }, 1000);
+}
+componentDidUpdate(){
+  setInterval(() => {
+    console.log('Atualizou!!!')
+  }, 1000);
+}
 
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-    state.nome= "José";
-    this.setState(state);
-  }
 
-  diminuir() {
-    let state = this.state;
-    if(state.contador = 0) {
-      alert("Ops chegou a 0")
-      return;
-    }
-    state.contador -= 1;
-    this.setState(state);
-  }
-
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <h1>Contador</h1>
-        {this.state.nome}
-
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
+        <h1>Meu projeto {this.state.hora}</h1>
       </div>
     );
   }
